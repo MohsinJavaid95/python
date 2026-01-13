90918cb4c7d648e0db4341fc9d63210e0bdc6db5
Mohsin Javaid <mohsinjavaid8@gmail.com>
2026-01-14 00:50:37 +0500

    python

---
+++ firstprogram.py
@@ -0,0 +1,56 @@
+# # # conditional statements
+
+# # age = 100
+# # print ("your age is:",age)
+
+# # if(age>=18 and age<=90):
+# #     print("you can vote")
+# # elif(age<18):
+# #     print("you can not cast a vote")
+# # else:
+# #     print("have a good day")
+
+
+# a=input("insert first value:")
+# b=input("insert 2nd value:")
+# print("first value is:",a)
+# print("second value is:",b)
+
+# a=int(a)
+# b=int(b)
+# c=input("enter operator:")
+
+# if(c=="+"):
+#     print("you selecte
+# 
+# 
+# d addition")
+#     print("your result is:",a+b)
+
+
+
+# age=input("please insert age:")
+# print("your age is:",age)
+# age=int(age)                #typecasting
+
+# if(age>=18):
+#     print("you can vote")
+# elif(age<18):
+#     print("you can not cast vote")
+
+
+a=input("insert first number:")
+a=int(a)  # typecasting
+b=input("insert 2nd number:")
+b=int(b)
+
+print("your numbers are",a,"and",b)
+opr=input("insert your operator")
+print("your operator is:",opr)
+
+if(opr=="+"):
+    print("your addition answer is",a+b)
+elif(opr=="-"):
+    print("subtraction answer is:",a-b)
+elif(opr=="/"):
+    print("division answer is:",a/b)
